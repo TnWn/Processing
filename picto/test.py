f7f1a817428907535a9d1d758fd89e82ef2aca8b
Wen <wenxtn@gmail.com>
2018-03-27 23:11:11 +1100

    final picto implementation

--- picto/test.py
+++ picto/test.py
@@ -2,24 +2,24 @@ import datetime
 
 def setup():
     size(1200,1200)
-    background(20,20,20)
+    background(80,80,80)
 
 def drawLines():
 
     stroke(255,255,255)
-    strokeWeight(20)
     strokeJoin(ROUND)
     strokeCap(ROUND)
 
     size = 50
     test = []
-    color_set = [[178,27,60],[255,250,162],[255,93,128],[54,163,204],[56,145,178]]
+    color_set = [[178,88,86],[255,255,144],[255,146,144],[115,167,204],[101,146,178]]
+    strokeWeight(20)
 
     oldx = int(random(1,3)) * 300
     print oldx
     oldy = int(random(1,3)) * 300
     print oldy
-    for i in range(0,10):
+    for i in range(0,6):
 
         rand_color = color_set[int(random(0,4))]
         print rand_color
@@ -43,38 +43,49 @@ def drawLines():
 
     for c in test:
 
-        fill(20,20,20)
+        fill(80,80,80)
 
         rand_color = color_set[int(random(0,4))]
         print rand_color
         stroke(rand_color[0], rand_color[1], rand_color[2])
+        strokeWeight(15)
 
         print c
         x = c[0]
         y =  c[1]
-        shape = int(random(6))
+        shape = int(random(7))
+
+        #Large size
         if (shape == 0):
             ellipse(x,y,(size*4),(size*4))
         elif (shape == 1):
             rect(x-(size*2),y-(size*2),(size*4),(size*4))
         elif (shape == 2):
             triangle(x-(size*2),y+(size*2),x+(size*2),y+(size*2),x,y-(size*2))
+        #medium size
         elif (shape == 3):
-            ellipse(x,y,(size*3),(size*3))
+            ellipse(x,y,(size*2),(size*2))
         elif (shape == 4):
             rect(x-(size*1.5),y-(size*1.5),(size*3),(size*3))
         elif (shape == 5):
             triangle(x-(size*1.5),y+(size*1.5),x+(size*1.5),y+(size*1.5),x,y-(size*1.5))
+        #small size
+        elif (shape == 5):
+            ellipse(x,y,(size*0.5),(size*0.5))
         elif (shape == 6):
+            rect(x-(size*0.5),y-(size*0.5),(size),(size))
+        elif (shape == 7):
             #do nothing
             print "nothing"
 
 def draw():
+
     drawLines()
 
-    time = datetime.datetime.now().strftime("%d_%m_%H_%M")
+    time = datetime.datetime.now().strftime("%d_%m_%H_%M_%S")
     print time
     fileName = "/pics/" + time + ".jpg"
     print fileName
     save(fileName)
+
     noLoop()
