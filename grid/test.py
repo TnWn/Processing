97ca7eb160448ca2c9816932178cd7e02301df0f
Wen <wenxtn@gmail.com>
2018-04-16 21:24:50 +1000

    added different 'hole' shapes

--- grid/test.py
+++ grid/test.py
@@ -1,6 +1,10 @@
 import datetime
 
 canvas_size = 1000
+rand_min = 100
+rand_neg = -300
+rand_max = 300
+c = color(144, 180, 187)
 
 def setup():
     size(canvas_size,canvas_size)
@@ -8,16 +12,26 @@ def setup():
     background(255)
     smooth()
     frameRate(30)
-    background(200,150,150)
+    background(c)
 
-#add variation in hole shapes
 def hole():
     noStroke()
-    c = color(200,150,150)
     fill(c)
     for y in range(0,5):
-        ellipse(random(0,canvas_size), random(0,canvas_size), random(100,500), random(100,500))
-
+        random_shape = int(random(0.5,4.5))
+        if (random_shape == 1):
+            ellipse(random(0,canvas_size), random(0,canvas_size), random(rand_min, rand_max), random(rand_min, rand_max))
+        if (random_shape == 2):
+            rect(random(0,canvas_size), random(0,canvas_size), random(rand_min, rand_max), random(rand_min, rand_max))
+        if (random_shape == 3):
+            random_x = random(0,canvas_size)
+            random_y = random(0,canvas_size)
+            triangle(random_x, random_y, random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max), random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max))
+        if (random_shape == 4):
+            random_x = random(0,canvas_size)
+            random_y = random(0,canvas_size)
+            quad(random_x, random_y, random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max), random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max), random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max))
+#maybe add variation to thickness & color
 def grid(x):
     stroke(1)
     for x in range(0,canvas_size, canvas_size/x):
@@ -25,10 +39,10 @@ def grid(x):
         line(0, x, canvas_size, x)
 
 def draw():
-    for x in range(0,10):
+    for x in range(0,500):
+        print x
         hole()
-        grid(int(random(0,100)))
-        #hole()
+        grid(int(random(1,100)))
     noLoop()
 
 def mouseClicked():
