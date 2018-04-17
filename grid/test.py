2f04b4fb74e4897255f7f757cda9aac55eca67ba
Wen <wenxtn@gmail.com>
2018-04-17 15:42:24 +1000

    rotate grids

--- grid/test.py
+++ grid/test.py
@@ -4,8 +4,10 @@ canvas_size = 1000
 rand_min = 100
 rand_neg = -300
 rand_max = 300
+margin = 100
 c = color(144, 180, 187)
 
+#figure out background & line color scheme
 def setup():
     size(canvas_size,canvas_size)
     strokeWeight(1)
@@ -14,10 +16,11 @@ def setup():
     frameRate(30)
     background(c)
 
+#rounded edges on shapes?
 def hole():
     noStroke()
     fill(c)
-    for y in range(0,5):
+    for y in range(0,6):
         random_shape = int(random(0.5,4.5))
         if (random_shape == 1):
             ellipse(random(0,canvas_size), random(0,canvas_size), random(rand_min, rand_max), random(rand_min, rand_max))
@@ -34,16 +37,25 @@ def hole():
 
 def grid(x):
     rotate(int(random(0,360)))
-    stroke(1)
-    for x in range(0,canvas_size, canvas_size/x):
-        line(x, 0, x, canvas_size)
-        line(0, x, canvas_size, x)
+    stroke(int(random(1,5)))
+    #for x in range(0 - margin, canvas_size + margin, canvas_size/x):
+    for x in range(-canvas_size, canvas_size, canvas_size/x):
+        wiggle = random(-30,30)
+        line(x + wiggle, 0 + wiggle, x + wiggle, canvas_size + wiggle)
+        line(0 + wiggle, x + wiggle, canvas_size + wiggle, x + wiggle)
 
 def draw():
-    for x in range(0,100):
+    '''
+    for x in range(0,1000):
         print x
         hole()
         grid(int(random(1,100)))
+    '''
+    for x in range(0,5000):
+        print x
+        hole()
+        grid(int(random(1,100)))
+    grid(int(random(1,100)))
     noLoop()
 
 def mouseClicked():
