9a48976007af48799e176189d97887ef4b78ba7d
Wen <wenxtn@gmail.com>
2018-04-17 22:57:07 +1000

    color scheme

--- grid/test.py
+++ grid/test.py
@@ -5,27 +5,26 @@ rand_min = 100
 rand_neg = -300
 rand_max = 300
 margin = 100
-c = color(144, 180, 187)
+bgc = color(255, 255, 245)
+grid_colors = [[225,90,80], [225,60,60], [194,100,100], [21,75,100], [12,90,80], [60,4,100]]
 
-#figure out background & line color scheme
 def setup():
     size(canvas_size,canvas_size)
-    strokeWeight(1)
-    background(255)
-    smooth()
     frameRate(30)
-    background(c)
+    background(bgc)
+    strokeCap(ROUND)
+    strokeCap(ROUND)
+    smooth(4)
 
-#rounded edges on shapes?
 def hole():
     noStroke()
-    fill(c)
-    for y in range(0,6):
+    fill(bgc)
+    for y in range(0,10):
         random_shape = int(random(0.5,4.5))
         if (random_shape == 1):
             ellipse(random(0,canvas_size), random(0,canvas_size), random(rand_min, rand_max), random(rand_min, rand_max))
         if (random_shape == 2):
-            rect(random(0,canvas_size), random(0,canvas_size), random(rand_min, rand_max), random(rand_min, rand_max))
+            rect(random(0,canvas_size), random(0,canvas_size), random(rand_min, rand_max), random(rand_min, rand_max),random(10,50))
         if (random_shape == 3):
             random_x = random(0,canvas_size)
             random_y = random(0,canvas_size)
@@ -36,21 +35,16 @@ def hole():
             quad(random_x, random_y, random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max), random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max), random_x + random(rand_neg , rand_max), random_y + random(rand_neg , rand_max))
 
 def grid(x):
-    rotate(int(random(0,360)))
-    stroke(int(random(1,5)))
-    #for x in range(0 - margin, canvas_size + margin, canvas_size/x):
+    strokeWeight(random(0.1,2))
+    random_color = int(random(0,len(grid_colors)))
+    stroke(grid_colors[random_color][0], grid_colors[random_color][1], grid_colors[random_color][2])
+    rotate(random(0,1))
     for x in range(-canvas_size, canvas_size, canvas_size/x):
         wiggle = random(-30,30)
         line(x + wiggle, 0 + wiggle, x + wiggle, canvas_size + wiggle)
         line(0 + wiggle, x + wiggle, canvas_size + wiggle, x + wiggle)
 
 def draw():
-    '''
-    for x in range(0,1000):
-        print x
-        hole()
-        grid(int(random(1,100)))
-    '''
     for x in range(0,5000):
         print x
         hole()
