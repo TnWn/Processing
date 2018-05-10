4186d01652a706bc9a64a2c91fbae5cf79480ee4
Wen <wenxtn@gmail.com>
2018-05-10 21:56:17 +1000

    make canvas 1x3 and autosave image

--- line/test.py
+++ line/test.py
@@ -1,31 +1,29 @@
 import datetime
 
-CANVAS_SIZE = 1000
+X_CANVAS_SIZE = 1000
+Y_CANVAS_SIZE = 3000
 X_MARGIN = 300
 Y_MARGIN = 50
 
 def setup():
-    size(CANVAS_SIZE,CANVAS_SIZE)
+    size(X_CANVAS_SIZE, Y_CANVAS_SIZE)
     strokeWeight(2)
     background(255)
     smooth()
     frameRate(5000)
     background(20,20,20)
     colorMode(HSB, 1, 1 ,1)
-    #rand_color = color_set[int(random(0,len(color_set)))]
-    #stroke(rand_color[0], rand_color[1], rand_color[2])
 
-x= random(X_MARGIN,CANVAS_SIZE-X_MARGIN)
-y= random(Y_MARGIN,CANVAS_SIZE-Y_MARGIN)
-px= random(X_MARGIN,CANVAS_SIZE-X_MARGIN)
-py= random(Y_MARGIN,CANVAS_SIZE-Y_MARGIN)
-#color_set = [[158,91,178], [255,233,181], [232,156,255], [104,204,165], [100,178,148], [178,98,91], [240,255,148], [255,164,156], [104,152,204], [108,142,178]]
-#color_set = [[255,0,0], [250,0,0], [245,0,0], [240,0,0], [235,0,0]]
+x= random(X_MARGIN,X_CANVAS_SIZE-X_MARGIN)
+y= random(Y_MARGIN,Y_CANVAS_SIZE-Y_MARGIN)
+px= random(X_MARGIN,X_CANVAS_SIZE-X_MARGIN)
+py= random(Y_MARGIN,Y_CANVAS_SIZE-Y_MARGIN)
 dir = random(0,360)
 limit = 0
+takeImage = 0
 h = 0
-s = 0
-b = 0
+s = 0.1
+b = 0.1
 
 def incrementColor():
     global h
@@ -33,13 +31,11 @@ def incrementColor():
     global b
     if (h>1):
         h = 0
-        s += 0.1
-        b += 0.1
-    elif (s>1):
-        s = 0
-        b = 0
+        if (s<0.8):
+            s += 0.05
+            b += 0.05
     else:
-        h += 0.001
+        h += 0.005
     stroke(h,s,b)
 
 def drawLine():
@@ -49,37 +45,48 @@ def drawLine():
     global py
     global dir
     global limit
+    global takeImage
     vel = int(random(0,10))
     px = x
     py = y
     x+= cos(radians(dir))*vel
     y+= sin(radians(dir))*vel
-    if ((y<Y_MARGIN) or (y>CANVAS_SIZE-Y_MARGIN)):
+    if ((y<Y_MARGIN) or (y>Y_CANVAS_SIZE-Y_MARGIN)):
         dir = -dir + random(-90,90)
         x+= (cos(radians(dir))*vel)
         y+= (sin(radians(dir))*vel)
         limit += 1
-    if ((x<X_MARGIN) or (x>CANVAS_SIZE-X_MARGIN)):
+    if ((x<X_MARGIN) or (x>X_CANVAS_SIZE-X_MARGIN)):
         dir = dir - (180 + random(-90,90))
         x+= (cos(radians(dir))*vel)
         y+= (sin(radians(dir))*vel)
         limit += 1
-    if (limit > 50):
-        x= random(X_MARGIN,CANVAS_SIZE-X_MARGIN)
-        y= random(Y_MARGIN,CANVAS_SIZE-Y_MARGIN)
+    #how long the line can wander away from the boundaries
+    if (limit > 300):
+        x= random(X_MARGIN,X_CANVAS_SIZE-X_MARGIN)
+        y= random(Y_MARGIN,Y_CANVAS_SIZE-Y_MARGIN)
         limit = 0
+        takeImage += 1
+        print takeImage
+        if (takeImage > 3):
+            saveImage()
+            takeImage = 0
     line(px,py,x,y)
 
 def draw():
     drawLine()
     incrementColor()
 
-def mouseClicked():
-    time = datetime.datetime.now().strftime("%d_%m_%H_%M")
+def saveImage():
+    print "saving image"
+    time = datetime.datetime.now().strftime("%d_%m_%H_%M_%S")
     print time
     fileName = "/pics/" + time + ".jpg"
     print fileName
     save(fileName)
+
+def mouseClicked():
+    saveImage()
     if (mouseButton == LEFT):
         print "END"
         noLoop()
