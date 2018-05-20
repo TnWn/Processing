61046e125123d46c115ffc899e2f808336a38963
Wen <wenxtn@gmail.com>
2018-05-20 17:14:39 +1000

    Make 'slice' size based on iterator - so the size stays consistent as the circle becomes bigger

--- platter/test.py
+++ platter/test.py
@@ -2,7 +2,6 @@ import datetime
 import math
 
 canvas_size = 1000
-#COLOR_LIST = [[179, 68, 11], [177, 47, 71], [147, 56, 108], [101, 70, 123], [59, 75, 113], [47, 72, 88]]
 COLOR_LIST = [[76, 0, 207], [196, 142, 0], [191, 28, 0], [1, 197, 201], [67, 199, 0], [0, 66, 207], [183, 196, 0], [0, 199, 126], [207, 0, 143]]
 BACKGROUND_COLOR = [20,20,20]
 
@@ -16,6 +15,8 @@ def setup():
     frameRate(30)
     background(BACKGROUND_COLOR[0], BACKGROUND_COLOR[1], BACKGROUND_COLOR[2])
 
+#Make the points for the circle, then call drawCircle to draw using point_list
+#ARGS: iterator - sets the size of the circle, larger iterator = larger circle
 def createCircle(iterator):
     point_list = []
     radius = 400 + (iterator * iterator * 200)
@@ -27,14 +28,16 @@ def createCircle(iterator):
         y = -sqrt(radius  - x ** 2)
         if not math.isnan(y):
             point_list.append([x,y])
-    for i in range(0,10):
+    for i in range(0,8):
         drawCircle(point_list,iterator)
 
 def drawCircle(point_list, iterator):
     random_color = int(random(0,len(COLOR_LIST)))
+    #MAKE STROKE SLIGHTLY DARKER THAN FILL? - USE HSL
     stroke(COLOR_LIST[random_color][0], COLOR_LIST[random_color][1], COLOR_LIST[random_color][2])
     fill(COLOR_LIST[random_color][0], COLOR_LIST[random_color][1], COLOR_LIST[random_color][2])
-    random_max =  int(random(0,len(point_list)/8))
+    random_max = int(random(1,len(point_list)))
+    random_max = random_max / iterator
     stroke_weight = 5
     beginShape()
     for j in range(0, random_max):
@@ -46,7 +49,7 @@ def drawCircle(point_list, iterator):
 
 def draw():
     translate(canvas_size/2, canvas_size/2)
-    for x in range(50,0,-1):
+    for x in range(40,0,-1):
         createCircle(x)
     noLoop()
     mouseClicked()
